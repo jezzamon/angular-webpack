0f5b3e463c6e7af243dd750e8aed3e71f55550ae
jezzamon <jezzamondev@gmail.com>
2017-02-17 19:39:37 -0500

    first commit

---
+++ src/vendor.ts
@@ -0,0 +1,12 @@
+import 'es6-shim';
+
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
+
+import '@angular/platform-browser';
+
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
